d15bc78e5c30e48bbbf772102a1bf542027ac041
hathbanger <hathbanger.ah@gmail.com>
2025-06-30 15:14:05 +0200

    set project id check

--- alkamist.sh
+++ alkamist.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+if [ -z "$SANDSHREW_PROJECT_ID" ]; then
+  echo "❌ Please export your SANDSHREW_PROJECT_ID before using Alkamist."
+  echo "Example:"
+  echo "  export SANDSHREW_PROJECT_ID=348ae3256c48c15cc99dcb056d2f78df"
+  return 1
+fi
+
 source "$HOME/.config/alkamist/functions.sh"
 echo "🔮 Alkamist loaded → available commands: decode, trace"
